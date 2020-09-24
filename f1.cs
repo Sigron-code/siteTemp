eeaf81a4659273b46094522dfd8618304bc217c9
Sigron <42572273+Sigron-code@users.noreply.github.com>
2020-09-24 18:10:00 +0300

    dudu1

---
+++ f1.cs
@@ -0,0 +1,37 @@
+class aaa
+{
+    int a;
+    float b;
+
+    aaa()
+    {
+        
+    }
+
+    int foo1()
+    {
+        return 5;
+    }
+
+}
+
+class bbb
+{
+    int b;
+    float bb;
+
+    bbb(int bval)
+    {
+        b=bb=bval;
+    }
+
+    int foo1()
+    {
+        return b;
+    }
+
+    }
+
+    
+
+
